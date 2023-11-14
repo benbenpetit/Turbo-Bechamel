e5da2af81680787b726a39fd547ccb3889099a44
Benoit PETIT <petitbenoit3@gmail.com>
2023-11-14 18:06:49 +0100

    scroll window

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import Rape from '@/components/Kitchen/Rape'
+import Kitchen from '@/assets/img/kitchen.jpg'
+import { useRef, useState } from 'react'
 
 const App = () => {
+  const [isDown, setIsDown] = useState(false)
+  const screenFirstRef = useRef<HTMLDivElement>(null)
+  const screenSecondRef = useRef<HTMLDivElement>(null)
+
+  const handleScroll = () => {
+    setIsDown(!isDown)
+    window.scrollTo({
+      top: (isDown ? 0 : screenSecondRef.current?.offsetHeight) || 0,
+      behavior: 'smooth'
+    })
+  }
+
   return (
-    <>
-      <Rape />
-    </>
+    <main onClick={handleScroll}>
+      <div ref={screenFirstRef} className='screen first'>
+        <Rape />
+      </div>
+      <div ref={screenSecondRef} className='screen second'></div>
+      <img className='background' src={Kitchen} alt='Cusine' />
+    </main>
   )
 }
 
